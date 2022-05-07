71247d769261e99755f2d63ae7a9f116176504eb
yosetc <ycozco@unsa.edu.pe>
2022-05-06 20:18:21 -0500

    2 new form added, added step row/item, and progrees bar, index.js file modified, var initialized & onclick function on button next1 added

---
+++ index.js
@@ -0,0 +1,17 @@
+var Form1 = document.getElementById("Form1");
+var Form2 = document.getElementById("Form2");
+var Form3 = document.getElementById("Form3");
+
+var Next1 = document.getElementById("next1");
+var Next2 = document.getElementById("next2");
+
+var Back1 = document.getElementById("back1");
+var Back2 = document.getElementById("back2");
+
+
+Next1.onclick = function() {
+    Form1.style.left = "-450px";
+    Form2.style.left = "40px";
+
+}
+
